f47d91f611f2beb320aa12b10588bf764e79d461
Mario Cannistrà <mariocannistra@gmail.com>
2016-05-29 21:30:05 +0200

    Revert "Revert "subfolder management for scans and cross platform compatibility changes""
    This reverts commit 6b5184c19021793529391c32a5407048596de447.

--- gainloop.py
+++ gainloop.py
@@ -88,23 +88,24 @@ for ggg in availgains:
 
 	scanname = scanname + "-g" + str( ggg ) + "-e" + datagathduration
 
-	completecmdstring = cmdstring + " -m " + scanname
+	completecmdstring = cmdstring + " -m " + sessiondate + os.sep + scanname
 
 	print('running scan with gain %s' % (ggg))
 	print(completecmdstring)
 
 	#run the scan and wait for completion
 	scanp = subprocess.Popen(completecmdstring, shell = True)
-	os.waitpid(scanp.pid, 0)
+	#os.waitpid(scanp.pid, 0)
+	scanp.wait()
 
 	#get event probability info
 	#process the scan adding probability info
 
-	chartcmdstring = "python postprocw.py " + scanname
+	chartcmdstring = "python postprocw.py " + scanname + " 0.0 0.0 " + sessiondate
 	#run gnuplot WITHOUT waiting for completion
 	genchrtp = subprocess.Popen(chartcmdstring, shell = True)
 
 	print('processing complete for scan with gain %s' % (ggg))
 
-chartcmdstring = "python findsessionrangew.py"
+chartcmdstring = "python findsessionrangew.py " + sessiondate
 genchrtp = subprocess.Popen(chartcmdstring, shell = True)
